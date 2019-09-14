a406f4c07eeb9b5818b405f0b46490854474332d
Lpablo <Lpablo>
2019-09-14 16:04:34 -0500

    practice finished

---
+++ pages/products/_id/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container">
+        <h2>
+            <center>you are selected a product {{ $route.params.id }}</center>
+        </h2>
+        
+        
+    </div>
+</template>
---
+++ pages/products/index.vue
@@ -0,0 +1,10 @@
+<<template>
+    <div class="container">
+        <h1>Page of products</h1>
+        <ul>
+            <nuxt-link to="products/1">First product</nuxt-link>
+            <nuxt-link to="products/2">Second product</nuxt-link>            
+        </ul>
+        
+    </div>
+</template>
